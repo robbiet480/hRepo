b2a490caa6d0fed5fca72a075b30b85c9054345f
Robbie Trencheny <me@robbiet.us>
2010-12-12 07:14:19 +0000

    about fixes

--- pages/about.php
+++ pages/about.php
@@ -21,20 +21,68 @@ if($slug == "about") {
 	<dd>PHP, MySQL, HTML, CSS</dd> 
 </dl> 
  
-<h4>Member 2</h4> 
+<h4>emirin</h4> 
 <p>Bla bla bla</p> 
  
 <img alt="Member 2" class="bioicon" src="http://placehold.it/57x57" /> 
 <dl class="bioinfo"> 
 	<dt>Title</dt> 
-	<dd>Something or other</dd> 
+	<dd>sql dev</dd> 
 	
 	<dt>IRC Nick</dt> 
-	<dd>blablabla</dd> 
+	<dd>emirin_</dd> 
 	
 	<dt>Coding languages</dt> 
-	<dd>A B C</dd> 
+	<dd>PHP, SQL</dd> 
 </dl>
+
+<h4>Alec Gorge</h4> 
+<p>High school student by day, programmer by night</p> 
+ 
+<img alt="Member 2" class="bioicon" src="http://placehold.it/57x57" /> 
+<dl class="bioinfo"> 
+	<dt>Title</dt> 
+	<dd>Programmer</dd> 
+	
+	<dt>IRC Nick</dt> 
+	<dd>alecgorge</dd> 
+	
+	<dt>Coding languages</dt> 
+	<dd>PHP, MySQL, HTML, CSS, Python, Objective-C, Javascript, Java, C#</dd> 
+</dl>
+
+<h4>Sturmeh</h4> 
+<p>Bla bla bla</p> 
+ 
+<img alt="Member 2" class="bioicon" src="http://placehold.it/57x57" /> 
+<dl class="bioinfo"> 
+	<dt>Title</dt> 
+	<dd>Programmer</dd> 
+	
+	<dt>IRC Nick</dt> 
+	<dd>Sturmeh</dd> 
+	
+	<dt>Coding languages</dt> 
+	<dd>Java, PHP, MySQL</dd> 
+</dl>
+
+<h4>Organik</h4> 
+<p>Bla bla bla</p> 
+ 
+<img alt="Member 2" class="bioicon" src="http://placehold.it/57x57" /> 
+<dl class="bioinfo"> 
+	<dt>Title</dt> 
+	<dd>Designer/Graphics</dd> 
+	
+	<dt>IRC Nick</dt> 
+	<dd>Organik</dd> 
+	
+	<dt>Coding languages</dt> 
+	<dd>Photoshop, CSS, HTML, SQL</dd> 
+</dl>
+
+
+
 EOT
 	);
 	
