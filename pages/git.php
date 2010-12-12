a00be20875a11cae89ce585413c983932cd69969
Alec Gorge <alecgorge@gmail.com>
2010-12-12 18:19:23 -0500

    title for git page

--- pages/git.php
+++ pages/git.php
@@ -1,12 +1,10 @@
 <?php
 
 if($slug == "git") {
+	Content::$forceTitle = "Git Update";
 	chdir(HR_ROOT);
 	Content::setContent("
 	<h1>Updating website from git...</h1>
 	<pre>".`echo Running git pull && git pull 2>&1`."</pre>
-
-
 	");
-	
 }
