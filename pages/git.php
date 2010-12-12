253339ebdedb0d66a33ccd6555adb814f984b52e
Alec Gorge <alecgorge@gmail.com>
2010-12-12 18:22:23 -0500

    rollback the rollback

--- pages/git.php
+++ pages/git.php
@@ -1,7 +1,7 @@
 <?php
 
 if($slug == "git") {
-	//Content::$forceTitle = "Git Update";
+	Content::$forcedTitle = "Git Update";
 	chdir(HR_ROOT);
 	Content::setContent("
 	<h1>Updating website from git...</h1>
