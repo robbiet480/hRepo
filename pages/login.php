0dfe32542e0b3e27df9902220b44cab8e0161f18
Luke Granger-Brown <github@lukegb.com>
2010-12-12 13:24:28 +0000

    Removed lingering &nbsp; from login.php, and created an example Apache2 alias config.

--- pages/login.php
+++ pages/login.php
@@ -5,7 +5,7 @@ if($slug == "login") {
 	Content::setContent(<<<EOT
 	<form action="#" method="POST">
 	<label for="username" style="font-size:12pt;font-weight:bold;">Username:</label><input style="font-size:18pt;" size="25" type="text" name="username" id="username"><br /><br />
-	&nbsp;<label for="password" style="font-size:12pt;font-weight:bold;">Password:</label><input style="font-size:18pt;" size="25" type="password" name="password" id="password"><br />
+	<label for="password" style="font-size:12pt;font-weight:bold;">Password:</label><input style="font-size:18pt;" size="25" type="password" name="password" id="password"><br />
 	<input type="submit" value="Login">
 	</form>
 
