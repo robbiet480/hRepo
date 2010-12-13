1890ce58aa9041dc8c6e1d150d10900a5bc19c66
Alec Gorge <alecgorge@gmail.com>
2010-12-13 17:16:33 -0500

    a slightly nicer login box

--- pages/login.php
+++ pages/login.php
@@ -4,6 +4,7 @@ $nav['login'] = array('url' => '/login', 'slug' => 'login', 'name' => 'Login', '
 if($slug == "login") {
 	$message = User::loginHandle();
 	Content::setContent(<<<EOT
+	<h2>Login</h2>
 	<form action="/login" method="post">$message
 		<div class="form-row">
 			<label for="username">Username</label>
