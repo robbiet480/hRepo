ce3492e544a026a49436432d1a1a8bf6c8055f65
Luke Granger-Brown <github@lukegb.com>
2010-12-21 14:16:24 +0000

    Seriously? Who didn't set the password box as a password box...
    or was there a reason for it...

--- pages/login.php
+++ pages/login.php
@@ -12,7 +12,7 @@ if($slug == "login") {
 		</div>
 		<div class="form-row">
 			<label for="password">Password</label>
-			<span><input type="text" name="password" id="password" /></span>
+			<span><input type="password" name="password" id="password" /></span>
 		</div>
 		<div class="form-row form-row-last">
 			<span><input type="submit" name="login" value="Login" /></span>
