5dcf586c5bcc1860781fd3659394bd09d1fb3042
Luke GB <github@lukegb.com>
2010-12-24 12:08:53 +0000

    ehehehe

--- pages/zzz-debugmode.php
+++ pages/zzz-debugmode.php
@@ -1,5 +1,5 @@
 <?php
 
 if ($_SESSION['debugmode']) {
-	Content::append('<h4>Debug details</h4><pre>'.print_r(Log::getLog()).'</pre>');
+	Content::append('<h4>Debug details</h4><pre>'.print_r(Log::getLog(), true).'</pre>');
 }
