8abe01bd02a1f624b5b2c650c6fddc8c2fc85b93
Ryan9104 <Ryan9104@gmail.com>
2010-12-25 16:08:41 -0600

    Just testing to see if this works.  Please ignore.
    -Ryan <3
    
    Signed-off-by: Ryan9104 <Ryan9104@gmail.com>

--- inc/sidebar.php
+++ inc/sidebar.php
@@ -10,7 +10,7 @@ class Sidebar {
 
 Sidebar::add("Most Downloaded Plugins", <<<EOT
 						<ol>
-							<li><a href="/plugins/sk89q/WorldEdit/">WorldEdit<span class="count"> (946)</span></a></li>
+							<li><a href="/plugins/sk89q/WorldEdit/">Cake<span class="count"> (946)</span></a></li>
 							<li><a href="/plugins/sk89q/WorldEdit/">WorldEdit<span class="count"> (946)</span></a></li>
 							<li><a href="/plugins/sk89q/WorldEdit/">WorldEdit<span class="count"> (946)</span></a></li>
 							<li><a href="/plugins/sk89q/WorldEdit/">WorldEdit<span class="count"> (946)</span></a></li>
