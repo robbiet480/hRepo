6f56f1cf5e9477e68f53dca4f91e26cd234da750
Luke GB <github@lukegb.com>
2010-12-26 23:57:37 +0000

    yay

--- inc/template.php
+++ inc/template.php
@@ -44,7 +44,15 @@ function nav() {
 	$totalpost = '';
 	foreach ($nav as $thisSlug => $vals)
 	{
-		if (($vals['visible'] !== false) && (($vals['loggedInOnly'] == 0) || ($vals['loggedInOnly'] == 1 && User::$isValid) || ($vals['loggedInOnly'] == -1 && !User::$isValid)))
+		if (
+				($vals['visible'] !== false) &&
+				(
+					($vals['loggedInOnly'] == 0) || // non-caring page
+					($vals['loggedInOnly'] == 1 && User::$isValid) || // only for logged in
+					($vals['loggedInOnly'] == -1 && !User::$isValid) // only for not logged in
+				)
+				&& (!isset($vals['minRole']) || (User::$role >= $vals['minRole']))
+		)
 		{
 			if (!isset($vals['extrapre']))
 				$vals['extrapre'] = '';
---
+++ pages/uploads/upload.php
@@ -0,0 +1,4 @@
+<?php
+
+$nav['upload'] = array('url' => '/upload', 'slug' => 'upload', 'name' => 'Upload New Plugin', 'loggedInOnly' => 1, 'minRole' => 1, 'weight' => 4, 'extrapre' => '', 'extrapost' => ''); // 1 for only logged in
+?>
