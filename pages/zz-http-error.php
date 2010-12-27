90798d0abe039dd1982b00b62073024a9d2c8907
Luke GB <github@lukegb.com>
2010-12-27 16:03:17 +0000

    *shuffle shuffle shuffle*

--- pages/zz-http-error.php
+++ pages/zz-http-error.php
@@ -1,18 +1,10 @@
 <?php
 
-if (empty(Content::$content)) {
+if (empty(Content::$content) && !isset($httpError)) {
 	$httpError = 404;
 }
 
-if ($httpError == 404) {
-	Content::$forcedTitle = "404 Error";
-	Content::$status = "404 Not Found";
-	Content::setcontent(<<<EOT
-	<h1>404 Error</h1>
-	<p>You suck.</p>
-EOT
-	);
-} else if ($httpError == 403) {
+ if ($httpError == 403) {
 	Content::$forcedTitle = "403 Unauthorised";
 	Content::$status = "403 Unauthorised";
 	$maybeLogin = '';
@@ -23,6 +15,14 @@ EOT
 	<h1>Access Denied</h1>
 	<p>You do not have the correct privileges to access this area.</p>
 	$maybeLogin
+EOT
+	);
+} else if ($httpError == 404) {
+	Content::$forcedTitle = "404 Error";
+	Content::$status = "404 Not Found";
+	Content::setcontent(<<<EOT
+	<h1>404 Error</h1>
+	<p>You suck.</p>
 EOT
 	);
 }
