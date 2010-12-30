622ee6f087390d2471aaaae4c78e40fd6343bf19
Luke GB <github@lukegb.com>
2010-12-30 19:33:21 +0000

    WORK DAMN IT WORK

--- pages/plugins/upload.php
+++ pages/plugins/upload.php
@@ -7,14 +7,12 @@ if ($slug == "upload")
 	$u = new XenForo_Model_User();
 	$pluginUserID = $u->getUserIdFromUser($u->getUserByName($pluginUsername));
 	$pluginName = $params[1];
-	$dbHandle = Database::getHandle();
-	$dbQuery = $dbHandle->prepare('SELECT pl.pname, pl.pauthor_id FROM plugins AS pl WHERE pl.pname = ? AND pl.pauthor_id = ?');
-	$dbRow = $dbQuery->execute(array($pluginName, $pluginUserID));
-	if ((User::$role == -1 || User::$uid != $pluginUserID) && (User::$role != User::ROLE_ADMIN))
+	$dbQuery = Database::select('plugins', 'pname, pauthor_id', array('pname = ? AND pauthor_id = ?', $pluginName, $pluginUserID));
+	if ((User::$role == User::ROLE_GUEST || User::$uid != $pluginUserID) && (User::$role != User::ROLE_ADMIN))
 	{
 		$httpError = 403;
 	}
-	else if ($dbRow->rowCount() != 1)
+	else if ($dbQuery->rowCount() != 1)
 	{
 		$httpError = 404;
 	}
