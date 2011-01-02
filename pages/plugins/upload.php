15acac1761530cf47fbf512eedb4de1d02a019bf
Luke GB <github@lukegb.com>
2011-01-02 22:00:22 +0000

    WEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE

--- pages/plugins/upload.php
+++ pages/plugins/upload.php
@@ -28,6 +28,7 @@ if ($slug == "upload")
 		$message .= Message::warning('Remember: if you want to upload a new version of a file, that file must be named EXACTLY the same.<br /><br />Example: I previously uploaded AwesomePlugin.jar - to upload a new version, you must ensure that it is named exactly the same.<br /><br /><b>File names cannot be changed once uploaded.</b>');
 		Content::addAdditionalCSS('uploadify.css');
 		Content::addAdditionalJS('jquery.uploadify.min.js');
+		Content::addAdditionalJS('upload.js');
 		$session = array(
 			'name' => session_name(),
 			'id' => session_id()
@@ -36,66 +37,12 @@ if ($slug == "upload")
 				<h1>Upload Files</h1>
 				$message
 				<div id="uploadBox"></div>
-				<form action="/uploadComplete/$params[0]/$params[1]/" method="POST" id="uploadFormForm">
+				<form action="/uploadComplete/{$params[0]}/{$params[1]}/" method="POST" id="uploadFormForm">
 				<div id="uploadFormArea"></div>
-				<button type="submit" id="bigSubmitButton" disabled="disabled">SUBMIT</button>
+				<input type="submit" id="bigSubmitButton" disabled="disabled">SUBMIT</button>
 				</form>
 				<script type="text/javascript">
-				jQuery(document).ready(function() {
-					jQuery('#uploadBox').uploadify(
-						{
-							'swf': '/static/images/uploadify/uploadify.swf',
-							'uploader': '/handleUpload/$params[0]/$params[1]/&{$session['name']}={$session['id']}&loadSessionFromGET=true',
-							'auto': true,
-							'multi': true,
-							'cancelImage': '/static/images/uploadify/uploadify-cancel.png',
-							'onSelect': function(file) {
-								makeTheRow = "<div id='"+file.id+"_details'><fieldset><legend>"+file.name+"</legend>";
-								makeTheRow = makeTheRow + "<div id='"+file.id+"_currentStatus'></div><br /><div id='"+file.id+"_newUploadForm'>";
-								makeTheRow = makeTheRow + "<label for='"+file.id+"_newname'>Filename:</label><input type='text' name='"+file.id+"_newname' id='"+file.id+"_newname' value='"+file.name+"' /></div>";
-								makeTheRow = makeTheRow + "<br /><label for='"+file.id+"_version'>File version:</label><input type='text' name='"+file.id+"_version' id='"+file.id+"_version' /><br />";
-								makeTheRow = makeTheRow + "<label for='"+file.id+"_changelog'>File Description/Changelog:</label><textarea name='"+file.id+"_changelog' id='"+file.id+"_changelog' style='width: 100%; height: 250px;'></textarea></fieldset></div>";
-								jQuery('#uploadFormArea').append(
-									makeTheRow
-								);
-							},
-							'onUploadStart': function(file) {
-								jQuery('#' + file.id + '_currentStatus').html("<div class='message message-info'><p>Upload beginning...</p></div>");
-								uploadInProgress = true;
-							},
-							'onUploadSuccess': function(file, data, response) {
-								jQuery('#' + file.id + '_currentStatus').html("<div class='message message-success'><p>Upload complete!</p></div>");
-								itemsSubmitted = itemsSubmitted + 1;
-							},
-							'onUploadError': function(file, errCode, errMsg) {
-								jQuery('#' + file.id + '_currentStatus').html("<div class='message message-error'><p>Upload failed...</p></div>");
-								if (errCode == SWFUpload.UPLOAD_ERROR.FILE_CANCELLED) {
-										jQuery('#' + file.id + '_details').remove();
-								}
-							},
-							'onUploadComplete': function(file, queue) {
-								uploadInProgress = (queue.queueLength > 0);
-								if (!uploadInProgress) {
-									$('#bigRedButton').removeAttr('disabled');
-								} else {
-									$('#bigRedButton').attr('disabled', 'disabled');
-								}
-							}
-						});
-						jQuery('#uploadFormForm').submit(function() {
-							if (uploadInProgress == true) {
-								alert('An upload is currently in progress. Please wait until it has completed before submitting the form.');
-								return false;
-							}
-							if (itemsSubmitted == 0) {
-								alert('You must upload something before you can submit this form.');
-								return false;
-							}
-							return true;
-						});
-					});
-					uploadInProgress = false;
-					itemsSubmitted = 0;
+	uploadURI = '/handleUpload/'+{$params[0]}+'/'+{$params[1]}+'/&{$session['name']}={$session['id']}&loadSessionFromGET=true';
 				</script>
 EOT
 				);
---
+++ pub/static/default/js/upload.js
@@ -0,0 +1,55 @@
+jQuery(document).ready(function() {
+	jQuery('#uploadBox').uploadify(
+	{
+		'swf': '/static/images/uploadify/uploadify.swf',
+		'uploader': uploadURI,
+		'auto': true,
+		'multi': true,
+		'cancelImage': '/static/images/uploadify/uploadify-cancel.png',
+		'onSelect': function(file) {
+			makeTheRow = "<div id='"+file.id+"_details'><fieldset><legend>"+file.name+"</legend>";
+			makeTheRow = makeTheRow + "<div id='"+file.id+"_currentStatus'></div><br /><div id='"+file.id+"_newUploadForm'>";
+			makeTheRow = makeTheRow + "<label for='"+file.id+"_newname'>Filename:</label><input type='text' name='"+file.id+"_newname' id='"+file.id+"_newname' value='"+file.name+"' /></div>";
+			makeTheRow = makeTheRow + "<br /><label for='"+file.id+"_version'>File version:</label><input type='text' name='"+file.id+"_version' id='"+file.id+"_version' /><br />";
+			makeTheRow = makeTheRow + "<label for='"+file.id+"_changelog'>File Description/Changelog:</label><textarea name='"+file.id+"_changelog' id='"+file.id+"_changelog' style='width: 100%; height: 250px;'></textarea></fieldset></div>";
+			jQuery('#uploadFormArea').append(
+			makeTheRow
+		);
+		},
+		'onUploadStart': function(file) {
+			jQuery('#' + file.id + '_currentStatus').html("<div class='message message-info'><p>Upload beginning...</p></div>");
+			uploadInProgress = true;
+		},
+		'onUploadSuccess': function(file, data, response) {
+			jQuery('#' + file.id + '_currentStatus').html("<div class='message message-success'><p>Upload complete!</p></div>");
+			itemsSubmitted = itemsSubmitted + 1;
+		},
+		'onUploadError': function(file, errCode, errMsg) {
+			jQuery('#' + file.id + '_currentStatus').html("<div class='message message-error'><p>Upload failed...</p></div>");
+			if (errCode == SWFUpload.UPLOAD_ERROR.FILE_CANCELLED) {
+				jQuery('#' + file.id + '_details').remove();
+			}
+		},
+		'onUploadComplete': function(file, queue) {
+			uploadInProgress = (queue.queueLength > 0);
+			if (!uploadInProgress) {
+				$('#bigSubmitButton').removeAttr('disabled');
+			} else {
+				$('#bigSubmitButton').attr('disabled', 'disabled');
+			}
+		}
+	});
+	jQuery('#uploadFormForm').submit(function() {
+		if (uploadInProgress == true) {
+			alert('An upload is currently in progress. Please wait until it has completed before submitting the form.');
+			return false;
+		}
+		if (itemsSubmitted == 0) {
+			alert('You must upload something before you can submit this form.');
+			return false;
+		}
+		return true;
+	});
+});
+uploadInProgress = false;
+itemsSubmitted = 0;
